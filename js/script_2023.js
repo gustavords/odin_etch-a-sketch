bd617d3a62f93a56ea75374791b1b8eaf544462b
gustavords <gustavords92@gmail.com>
2023-04-26 14:15:09 -0400

    Add reset, random and rainbow button functionality

--- js/script_2023.js
+++ js/script_2023.js
@@ -8,14 +8,18 @@ const brushPicker = document.getElementById(`brushPkr`);
 const bgPicker = document.getElementById(`bgPkr`);
 const eraserToggle = document.getElementById(`eraserTgl`);
 
+const toRGBArray = rgbStr => rgbStr.match(/\d+/g).map(Number);
+const eraserColour = () => { return bgPicker.value };
+const eraserToggleChecked = () => { return eraserToggle.checked };
+const randomRGBValue = () => { return Math.floor(Math.random() * 254) }; //all colours
+const isRainbowChecked = () => { return rainbowBtn.checked };
+
 //on start up
 let chosenColour = brushPicker.value;
 let chosenBgColour = bgPicker.value;
-const eraserColour = () => { return bgPicker.value };
-const eraserToggleChecked = () => { return eraserToggle.checked };
+let rainbowBtn = document.getElementById(`rainbowCheckbox`);
 sliderLbl.textContent = `${slider.value} X ${slider.value}`;
 createGrid();
-
 
 //resize grid event
 slider.addEventListener(`input`, () => {
@@ -24,13 +28,106 @@ slider.addEventListener(`input`, () => {
     createGrid();
 });
 
+//paint cell events
+grid.addEventListener("mousedown", (e) => {
+    colourCell(e);
+    grid.addEventListener("mouseover", colourCell);
+    pauseEvent(e);
+});
+grid.addEventListener(`mouseup`, (e) => {
+    grid.removeEventListener("mouseover", colourCell, { once: true });
+    pauseEvent(e);
+});
+
+/**
+ * TODO: make so function actually pain rather than colourCell function
+ */
+rainbowBtn.addEventListener(`change`, () => {
+    if (eraserToggleChecked() == true) eraserToggle.click();
+});
+
+//brush colour change button
+brushPicker.addEventListener("change", (e) => {
+    changeColorPicker(e);
+    resetCheckedButtons();
+}, false);
+
+/**
+ * TODO: figure out a way to keep the colors of brush in history to not be changed with background (remembers 5 colours???)
+ */
+//background colour change button
+bgPicker.addEventListener("change", (e) => {
+
+    resetCheckedButtons();
+
+    const cells = document.getElementsByClassName(`cell`);
+    changeBgColorPicker(e);
+
+    for (let cell of cells) {
+
+        let cellColor = cell.style.backgroundColor;
+        const rgbArr = toRGBArray(cellColor)
+        let hexValOfCellBkgCol = rgbToHex(rgbArr[0], rgbArr[1], rgbArr[2]);
+
+
+        if (hexValOfCellBkgCol == chosenColour) {
+            cell.style.backgroundColor = chosenColour;
+        }
+        else {
+            cell.style.backgroundColor = chosenBgColour;
+
+        }
+    }
+}, false);
+
+//random color button
+btnGroup[4].onclick = () => {
+    resetCheckedButtons();
+    chosenColour = `rgb(${randomRGBValue()},${randomRGBValue()},${randomRGBValue()})`;
+
+    const rgbArr = toRGBArray(chosenColour)
+    let hexVal = rgbToHex(rgbArr[0], rgbArr[1], rgbArr[2]);
+    // console.log(hexVal);
+    brushPicker.value = `${hexVal}`;
+    console.log(chosenColour);
+};
+
+//eraser brush toggle button event
+eraserToggle.addEventListener(`change`, () => {
+
+    if (eraserToggle.checked == true) {
+        chosenColour = eraserColour();
+        if (isRainbowChecked() == true) {
+            rainbowBtn.click();
+        }
+    }
+    if (eraserToggle.checked == false) {
+        chosenColour = brushPicker.value;
+    }
+
+});
+
+//erase background button event
+btnGroup[6].onclick = () => {
+    resetCheckedButtons();
+    eraseAll();
+};
+
+//reloads page
+btnGroup[7].onclick = () => {
+    location.reload();
+}
+
+
 /**TODO:
  * waring before resizing
  *    -- maybe do a hover thing where it warns you
  */
 function createGrid() {
     //resets eraser toggle
-    if (eraserToggleChecked() == true) eraserToggle.click();
+    // if (eraserToggleChecked() == true) eraserToggle.click();
+    resetCheckedButtons();
+
     //creates row
     for (let i = 0; i < slider.value; i++) {
         let row = document.createElement(`div`);
@@ -65,19 +162,6 @@ function removeRows() {
     });
 }
 
-
-//paint cell events
-grid.addEventListener("mousedown", (e) => {
-    colourCell(e);
-    grid.addEventListener("mouseover", colourCell);
-    pauseEvent(e);
-});
-
-grid.addEventListener(`mouseup`, (e) => {
-    grid.removeEventListener("mouseover", colourCell, { once: true });
-    pauseEvent(e);
-});
-
 //stops propagations, ∴ stopping mouse drag event
 function pauseEvent(e) {
     if (e.stopPropagation) e.stopPropagation();
@@ -86,25 +170,26 @@ function pauseEvent(e) {
     e.returnValue = false;
     return false;
 }
+
+//also does the RAINBOW function
 function colourCell(e) {
     e.target.style.backgroundColor = chosenColour;
+    if (isRainbowChecked()) {
+        e.target.style.backgroundColor = `rgb(${randomRGBValue()},${randomRGBValue()},${randomRGBValue()})`;
+    }
 }
 
-//eraser toggle button event
-eraserToggle.addEventListener(`change`, () => {
-    if (eraserToggle.checked == true) {
-        chosenColour = eraserColour();
-    }
-    if (eraserToggle.checked == false) {
-        chosenColour = brushPicker.value;
-    }
-});
+//resets checkbox button
+function resetCheckedButtons() {
+    if (eraserToggleChecked() == true) {
+        eraserToggle.click();
+    };
 
-//erase background button event
-btnGroup[4].onclick = () => {
-    if (eraserToggleChecked() == true) eraserToggle.click();
-    eraseAll();
-};
+    if (isRainbowChecked() == true) {
+        rainbowBtn.click();
+    }
+    return;
+}
 
 //erases to current background color
 function eraseAll() {
@@ -115,55 +200,10 @@ function eraseAll() {
     });
 }
 
-//brush colour change button
-brushPicker.addEventListener("change", changeColorPicker, false);
-
-// let previousBrushColour = [];
-// previousBrushColour.push(chosenColour);
-// console.log(previousBrushColour);
-
-// previousBrushColour.push(chosenColour);
-// console.log(previousBrushColour);
-
-// if(previousBrushColour.includes(chosenBgColour)){
-//     console.log(`here`)
-// }
-
-/**
- * TODO: figure out a way to keep the colors of brush in history to not be changed with background
- */
-//background colour change button
-bgPicker.addEventListener("change", (e) => {
-
-    if (eraserToggleChecked() == true) eraserToggle.click();
-
-    const cells = document.getElementsByClassName(`cell`);
-    changeBgColorPicker(e);
-
-
-    for (let cell of cells) {
-
-        let cellColor = cell.style.backgroundColor;
-        const rgbArr = toRGBArray(cellColor)
-        let hexValOfCellBkgCol = rgbToHex(rgbArr[0], rgbArr[1], rgbArr[2]);
-
-
-        if (hexValOfCellBkgCol == chosenColour) {
-            cell.style.backgroundColor = chosenColour;
-        }
-        else {
-            cell.style.backgroundColor = chosenBgColour;
-
-        }
-    }
-}, false);
-
-
 /**
  * all of this is because theres no rgb to hex conversion in js
  * and html outputs different color values from style.background and input buttons 
  */
-const toRGBArray = rgbStr => rgbStr.match(/\d+/g).map(Number);
 
 function componentToHex(c) {
     var hex = c.toString(16);
@@ -181,7 +221,3 @@ function changeColorPicker(event) {
 function changeBgColorPicker(event) {
     chosenBgColour = event.target.value;
 }
-
-// function updateFirst(event) {
-//     return event.target.value;
-// }
